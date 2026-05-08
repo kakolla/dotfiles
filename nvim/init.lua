22f6c1c0031fb2a66c647e2bafd8281033427310
kakolla <abhishekkakolla1@gmail.com>
2026-05-07 22:32:06 -0700

    black focused window and mru buffer list

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,6 +18,14 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
 
+-- CUSTOM
+-- after colorscheme loads, set bg to black on focus window
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = function()
+    local hl = vim.api.nvim_get_hl(0, { name = 'Normal' })
+    vim.api.nvim_set_hl(0, 'Normal', { fg = hl.fg, bg = 'black' })
+  end,
+})
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
@@ -490,8 +498,12 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
+      vim.keymap.set(
+        'n',
+        '<leader><leader>',
+        function() builtin.buffers { sort_mru = true, sorting_strategy = 'descending' } end,
+        { desc = '[ ] Find existing buffers' }
+      )
       -- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
       -- it is better explained there). This allows easily switching between pickers if you prefer using something else!
       vim.api.nvim_create_autocmd('LspAttach', {
